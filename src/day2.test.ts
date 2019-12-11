736b8d3113ff628824ecbca3b3ce8aa2e6e66013
Charlie Stanard <charlie.stanard@fourwindsinteractive.com>
2019-12-11 09:24:32 -0700

    Adding tests for Day 2/Part 1

--- src/day2.test.ts
+++ src/day2.test.ts
@@ -1,5 +1,45 @@
 import { input } from "./day2-input";
-import { computer } from "./day2";
+import { computer, executeProgram } from "./day2";
+
+describe("executeProgram", () => {
+  const finalResult: Array<number> = [1, 1, 5, 3, 2, 3, 0, 3, 1, 0, 1, 3];
+
+  it("should use correct opcode for addition", () => {
+    const program: Array<number> = finalResult.slice(0, 4);
+    expect(executeProgram(program, finalResult)).toEqual([
+      1,
+      1,
+      5,
+      4,
+      2,
+      3,
+      0,
+      3,
+      1,
+      0,
+      1,
+      3
+    ]);
+  });
+
+  it("should use correct opcode for multiplication", () => {
+    const program: Array<number> = finalResult.slice(4, 8);
+    expect(executeProgram(program, finalResult)).toEqual([
+      1,
+      1,
+      5,
+      4,
+      2,
+      3,
+      0,
+      3,
+      1,
+      0,
+      1,
+      3
+    ]);
+  });
+});
 
 describe("computer", () => {
   it("should return correct result in position 0", () => {
